0f84c6eb3df3e0a82bd4dac4eaec67bda1b78a18
Ruslan <suleimanovrn@gmail.com>
2025-02-05 00:47:18 +0300

    Update avatars.py

--- avatars.py
+++ avatars.py
@@ -9,15 +9,15 @@ width, height = image.size
 shift = 10
 half_shift = shift // 2
 
-red_left_crop = red.crop((shift, 0, width, height))  # Режем слева
-red_both_crop = red.crop((half_shift, 0, width - half_shift, height))  # Режем по бокам
-red_blend = Image.blend(red_left_crop, red_both_crop, alpha=0.5)  # Смешиваем
+red_left_crop = red.crop((shift, 0, width, height)) 
+red_both_crop = red.crop((half_shift, 0, width - half_shift, height))  
+red_blend = Image.blend(red_left_crop, red_both_crop, alpha=0.5)  
 
-blue_right_crop = blue.crop((0, 0, width - shift, height))  # Режем справа
-blue_both_crop = blue.crop((half_shift, 0, width - half_shift, height))  # Режем по бокам
-blue_blend = Image.blend(blue_right_crop, blue_both_crop, alpha=0.5)  # Смешиваем
+blue_right_crop = blue.crop((0, 0, width - shift, height))  
+blue_both_crop = blue.crop((half_shift, 0, width - half_shift, height))  
+blue_blend = Image.blend(blue_right_crop, blue_both_crop, alpha=0.5)  
 
-green_cropped = green.crop((half_shift, 0, width - half_shift, height))  # Режем по бокам
+green_cropped = green.crop((half_shift, 0, width - half_shift, height))  
 
 shifted_image = Image.merge("RGB", (red_blend, green_cropped, blue_blend))
 
